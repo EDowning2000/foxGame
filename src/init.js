c3fa02d49e7af98b13f11ce411e18192c0d2d4a7
Emerson Downing <emersondowning@gmail.com>
2021-01-16 22:12:59 -0500

    added the first state for the game as well as the init function to get everything started off

--- src/init.js
+++ src/init.js
@@ -1,5 +1,19 @@
+import gameState from "./gameState";
 const TICK_RATE = 3000;
 
-function tick() {
-  console.log("tick", Date.now());
+async function init() {
+  console.log("starting game");
+  let nextTimeToTick = Date.now();
+
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      gameState.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+    requestAnimationFrame(nextAnimationFrame);
+  }
+  requestAnimationFrame(nextAnimationFrame);
 }
+init();
