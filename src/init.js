289de3e985335bc93c72d04a2594826a8ee16650
Emerson Downing <emersondowning@gmail.com>
2021-01-18 20:50:44 -0500

    added some comments as well as the constants file

--- src/init.js
+++ src/init.js
@@ -1,12 +1,16 @@
 import gameState from "./gameState";
-const TICK_RATE = 3000;
+import { TICK_RATE } from "./constants";
+import initButtons from "./buttons";
 
 async function init() {
   console.log("starting game");
+  initButtons(game.handleUserAction);
+
   let nextTimeToTick = Date.now();
 
   function nextAnimationFrame() {
     const now = Date.now();
+    //sets the initial clock, and then adds 3 seconds, and then when the clock gets to the 3 seconds in the future it restarts
 
     if (nextTimeToTick <= now) {
       gameState.tick();
