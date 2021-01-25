a8073d7f48d2c2acb363b31613e420db6913001b
Emerson Downing <emersondowning@gmail.com>
2021-01-25 09:52:56 -0500

    got my ui interaction done with the buttons working correctly

--- src/init.js
+++ src/init.js
@@ -1,23 +1,22 @@
-import gameState from "./gameState";
-import { TICK_RATE } from "./constants";
+import game from "./gameState";
 import initButtons from "./buttons";
+import { TICK_RATE } from "./constants";
 
 async function init() {
   console.log("starting game");
   initButtons(game.handleUserAction);
 
   let nextTimeToTick = Date.now();
-
   function nextAnimationFrame() {
     const now = Date.now();
-    //sets the initial clock, and then adds 3 seconds, and then when the clock gets to the 3 seconds in the future it restarts
-
     if (nextTimeToTick <= now) {
-      gameState.tick();
+      game.tick();
       nextTimeToTick = now + TICK_RATE;
     }
     requestAnimationFrame(nextAnimationFrame);
   }
-  requestAnimationFrame(nextAnimationFrame);
+
+  nextAnimationFrame();
 }
+
 init();
